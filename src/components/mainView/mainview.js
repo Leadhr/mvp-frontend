e75fb56767a52c2c9ed917df6baa452988d90ff9
Leadhr <mhgamble1@gmail.com>
2020-08-05 10:00:30 -0500

    top level components

---
+++ src/components/mainView/mainview.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function MainView() {
+  return (<div className="mainView col">
+      <p>im main view</p>
+  </div>)
+}
---
+++ src/components/secondView/secondView.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SecondView() {
+    return (<div className="secondView col"><p>im second view</p></div>);
+}
