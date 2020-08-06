4edea6d6c35cb20268ece1bfdde59f4a7d0de40d
mhgamble1 <mhgamble1@gmail.com>
2020-08-06 13:05:28 -0500

    initialized TeamWrapper component

---
+++ src/components/TeamWrapper/TeamWrapper.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Team from "../Team/Team"
+
+export default function TeamWrapper() {
+    return (
+        <div className="TeamWrapper">
+            <Team />
+            <Team />
+            <Team />
+        </div>
+    )
+}
