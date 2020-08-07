70a14c53a516993ba330322726ff3d954abacf5b
mhgamble1 <mhgamble1@gmail.com>
2020-08-07 14:43:18 -0500

    refactored individual view to be simple and clear

--- src/components/WorkStylesIndividualView/WorkStylesIndividualView.js
+++ src/components/WorkStylesIndividualView/WorkStylesIndividualView.js
@@ -1,13 +1,27 @@
 import React from "react";
-import MainView from "../MainView/MainView";
-import SecondView from "../SecondView/SecondView";
+import Head from "../Head/Head";
+import Viz from "../Viz/Viz";
+import Avatar from "../Avatar/Avatar";
+import ThreePlot from "../ThreePlot/ThreePlot";
+import AttributeBlock from "../AttributeBlock/AttributeBlock";
+import LearnMore from "../LearnMore/LearnMore";
+import Search from "../Search/Search";
 
 export default function WorkStylesIndividualView() {
   return (
     <div className="WorkStylesIndividualView col">
       <div className="row">
-        <MainView context="workstylesindividual" />
-        <SecondView context="workstylesindividual" />
+        <div className="MainView col">
+          <Head />
+          <Viz />
+        </div>
+        <div className="SecondView col">
+          <Avatar />
+          <Search />
+          <ThreePlot />
+          <AttributeBlock />
+          <LearnMore />
+        </div>
       </div>
     </div>
   );
