566ce3f4ab17e58e96c7bdf6e5b73852fddcd314
mhgamble1 <mhgamble1@gmail.com>
2020-08-07 00:05:39 -0500

    placeholder changes

--- src/components/CultureBox/CultureBox.js
+++ src/components/CultureBox/CultureBox.js
@@ -6,6 +6,7 @@ import CultureActual from "../CultureActual/CultureActual";
 export default function CultureBox() {
     return (
         <div className="CultureBox">
+            <p>im culture box</p>
             <CultureIntended />
             <CultureActual />
         </div>
