dc4f9541f532b41d2d16a59dcd13f752e581d61f
mhgamble1 <mhgamble1@gmail.com>
2020-08-07 14:43:50 -0500

    refactored company view to be simple and clean

--- src/components/WorkStylesCompanyView/WorkStylesCompanyView.js
+++ src/components/WorkStylesCompanyView/WorkStylesCompanyView.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import MainView from "../MainView/MainView"
-import SecondView from "../SecondView/SecondView"
+import React from "react";
+import Head from "../Head/Head";
+import Viz from "../Viz/Viz";
+import Avatar from "../Avatar/Avatar";
+import ThreePlot from "../ThreePlot/ThreePlot";
+import AttributeBlock from "../AttributeBlock/AttributeBlock";
+import LearnMore from "../LearnMore/LearnMore";
 
 export default function WorkStylesCompanyView() {
-    return (
-        <div className="WorkStylesCompanyView col">
-          <div className="row">
-            <MainView context="workstylescompany"/>
-            <SecondView context="workstylescompany"/>
-          </div>
+  return (
+    <div className="WorkStylesCompanyView col">
+      <div className="row">
+        <div className="MainView col">
+          <Head />
+          <Viz />
         </div>
-      );
+        <div className="SecondView col">
+          <Avatar />
+          <ThreePlot />
+          <AttributeBlock />
+          <LearnMore />
+        </div>
+      </div>
+    </div>
+  );
 }
