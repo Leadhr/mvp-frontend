80ad42f2e70199a0590990294b240bad7f1de579
mhgamble1 <mhgamble1@gmail.com>
2020-08-07 00:05:16 -0500

    placeholder changes

--- src/components/Attribute/Attribute.js
+++ src/components/Attribute/Attribute.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function Attribute() {
-    return (
-        <div className="Attribute">
-            <p>im an attribute</p>
-        </div>
-    )
+  return (
+    <div className="Attribute">
+      <h1>im an attribute</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
 }
