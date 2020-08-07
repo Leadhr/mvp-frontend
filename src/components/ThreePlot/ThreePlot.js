933e8ad026855cab95cebdea10042664be1cdade
mhgamble1 <mhgamble1@gmail.com>
2020-08-07 00:07:16 -0500

    placeholder changes

--- src/components/ThreePlot/ThreePlot.js
+++ src/components/ThreePlot/ThreePlot.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ThreePlot() {
     return (
         <div className="ThreePlot">
-            <p>im three plot</p>
+            <p>im the three plot graphic! i might become four plot</p>
         </div>
     )
 }
