e9e6f068857b954dbeb7abb8762f0c73870ebeba
mhgamble1 <mhgamble1@gmail.com>
2020-08-10 15:45:33 -0500

    added copy

---
+++ src/components/ReferenceHead/ReferenceHead.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ReferenceHead() {
+  return (
+    <div className="ReferenceHead">
+      <ul className="nav justify-content-start HeadNav">
+        <li className="nav-item">
+          <a className="nav-link active" href="">
+            Definitions
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="">
+            The Model
+          </a>
+        </li>
+      </ul>
+      <p>
+        These styles reflect the types of thoughts and behavior that tend to
+        give us energy, help us achieve our goals, and shape how we see
+        ourselves. All eight styles are equally valid; we each display all eight
+        styles in one form or another. To learn more about each individual
+        style, read below.
+      </p>
+    </div>
+  );
+}
