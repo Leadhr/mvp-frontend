277fe2463d03469c8631ba55f4bf32e778e0ce2d
mhgamble1 <mhgamble1@gmail.com>
2020-08-10 15:45:07 -0500

    added copy

--- src/components/CultureActual/CultureActual.js
+++ src/components/CultureActual/CultureActual.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function CultureActual() {
     return (
         <div className="CultureActual">
-            <h1>im actual title</h1>
-            <p>im actual text
+            <h1>Actual: Achieving</h1>
+            <p>include blurb based on each based on call to table for appropriate business strategy associated with this culture type
             </p>
         </div>
     )
