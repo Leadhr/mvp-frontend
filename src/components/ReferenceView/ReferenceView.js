77dd01c756992617433124d21043422de615fcdb
mhgamble1 <mhgamble1@gmail.com>
2020-08-10 15:45:45 -0500

    added copy

--- src/components/ReferenceView/ReferenceView.js
+++ src/components/ReferenceView/ReferenceView.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReferenceText from "../ReferenceText/ReferenceText";
+import ReferenceHead from "../ReferenceHead/ReferenceHead";
 
 export default function ReferenceView(props) {
   return (
     <div className="MainView col">
       <div className="row">
         <div className="MainView col">
+          <ReferenceHead />
           <ReferenceText />
         </div>
         <div className="SecondView col"></div>
