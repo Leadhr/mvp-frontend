4a78ef8b85e35ed8ed215a960ef6b22aa5146ad7
mhgamble1 <mhgamble1@gmail.com>
2020-08-20 14:21:44 -0500

    pruned unused imports

--- src/views/Candidates.js
+++ src/views/Candidates.js
@@ -19,23 +19,17 @@ import React from "react";
 
 // reactstrap components
 import {
-  Badge,
+ 
   Card,
   CardHeader,
   CardFooter,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  DropdownToggle,
   Media,
   Pagination,
   PaginationItem,
   PaginationLink,
-  Progress,
   Table,
   Container,
   Row,
-  UncontrolledTooltip,
 } from "reactstrap";
 // core components
 import Header from "components/Headers/Header.js";
@@ -225,7 +219,7 @@ class Candidates extends React.Component {
               </Card>
             </div>
           </Row>
-          </Container>
+        </Container>
       </>
     );
   }
