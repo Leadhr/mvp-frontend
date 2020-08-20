cad1192dbdefea6b62b25ac5ed5a1b8617b618c1
mhgamble1 <mhgamble1@gmail.com>
2020-08-20 14:21:15 -0500

    removed demo routes

--- src/routes.js
+++ src/routes.js
@@ -16,12 +16,10 @@
 
 */
 import Index from "views/Index.js";
-import Profile from "views/examples/Profile.js";
-import Maps from "views/examples/Maps.js";
+
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
-import Tables from "views/examples/Tables.js";
-import Icons from "views/examples/Icons.js";
+
 import Culture from "views/Culture.js";
 import MyTeam from "views/MyTeam";
 import Candidates from "views/Candidates";
