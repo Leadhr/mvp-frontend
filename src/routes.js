28dff6c1eb4ec9526747249c107c5dea44331217
mhgamble1 <mhgamble1@gmail.com>
2020-08-21 11:16:15 -0500

    updated imports for login and register

--- src/routes.js
+++ src/routes.js
@@ -16,10 +16,8 @@
 
 */
 import Index from "views/Index.js";
-
-import Register from "views/examples/Register.js";
-import Login from "views/examples/Login.js";
-
+import Register from "views/Register.js";
+import Login from "views/Login.js";
 import Culture from "views/Culture.js";
 import MyTeam from "views/MyTeam";
 import Candidates from "views/Candidates";
