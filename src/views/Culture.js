a100d9610e562e1cc40c06393a9d266bc6ec4e1b
mhgamble1 <mhgamble1@gmail.com>
2020-08-21 14:47:35 -0500

    removed placeholder data

--- src/views/Culture.js
+++ src/views/Culture.js
@@ -8,40 +8,7 @@ import { Line } from "react-chartjs-2";
 import { chartOptions, parseOptions, chartExample1 } from "variables/charts.js";
 
 class Culture extends React.Component {
-  data = {
-    labels: [
-      "Achieving",
-      "Caring",
-      "Commanding",
-      "Considering",
-      "Enjoying",
-      "Exploring",
-      "Organizing",
-      "Structuring",
-    ],
-    datasets: [
-      {
-        label: "You",
-        backgroundColor: "rgba(179,181,198,0.2)",
-        borderColor: "rgba(179,181,198,1)",
-        pointBackgroundColor: "rgba(179,181,198,1)",
-        pointBorderColor: "#fff",
-        pointHoverBackgroundColor: "#fff",
-        pointHoverBorderColor: "rgba(179,181,198,1)",
-        data: [65, 59, 90, 81, 56, 55, 40, 33],
-      },
-      {
-        label: "Average Sales Team Member",
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
-        pointBackgroundColor: "rgba(255,99,132,1)",
-        pointBorderColor: "#fff",
-        pointHoverBackgroundColor: "#fff",
-        pointHoverBorderColor: "rgba(255,99,132,1)",
-        data: [28, 48, 40, 19, 96, 27, 100, 80],
-      },
-    ],
-  };
+  
 
   componentWillMount() {
     if (window.Chart) {
